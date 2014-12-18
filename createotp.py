66e5972769d542f1cd1093630cdc3aa64866790a
Son Nguyen <ngocson2vn@gmail.com>
2014-12-18 12:31:22 +0900

    Update

--- createotp.py
+++ createotp.py
@@ -5,15 +5,18 @@ import hashlib
 
 from hotpie import HOTP, TOTP
 
-key = 'ngocson2vn@gmail.comHDECHALLENGE003'
+key = 'ngocson2vn@gmail.com'
 t1 = TOTP(key, digits=10)      # <time-based-value>
-print t1
+print "totp1:%s length:%d" % (t1, len(str(t1)))
 
-secret = base64.b32encode('ngocson2vn@gmail.comHDECHALLENGE003')
-totp = pyotp.TOTP(secret, 10, hashlib.sha512)
+secret = base64.b32encode(key)
+totp = pyotp.TOTP(secret, 10, hashlib.sha512, interval=30)
 t2 = totp.now()
 
-print t2
-text = 'ngocson2vn@gmail.com:%s' % t1
-print text
-print base64.b64encode(text)
+print "totp2:%s length:%d" % (t2, len(str(t2)))
+
+
+if str(t1) == str(t2):
+	text = 'ngocson2vn@gmail.com:%s' % t1
+	print text
+	print base64.b64encode(text)
